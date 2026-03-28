c6d348a80b63f99c5b55c1ec2b88bc652f8e53cd
arnenys89 <63259576+arnenys89@users.noreply.github.com>
2026-03-28 19:35:05 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
