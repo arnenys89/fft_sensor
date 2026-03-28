b6442519635587a01573fb4762a3a0b582ddbac5
arnenys89 <63259576+arnenys89@users.noreply.github.com>
2026-03-28 19:36:02 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,40 +1,54 @@
 from homeassistant.components.sensor import SensorEntity
-from collections import deque
-from .fft import dominant_frequency
-
-WINDOW_SIZE = 128  # aantal samples
-SAMPLING_RATE = 1  # Hz (aanpassen!)
-
 class FFTSensor(SensorEntity):
-    def __init__(self, source_entity):
+    def __init__(self, source_entity, window_size, sampling_rate):
         self._source = source_entity
-        self._buffer = deque(maxlen=WINDOW_SIZE)
+        self._buffer = deque(maxlen=window_size)
+        self._sampling_rate = sampling_rate
+
         self._state = None
         self._amplitude = None
+        self._freqs = None
+        self._fft_vals = None
+
+        self._attr_name = f"FFT {source_entity}"
 
     async def async_added_to_hass(self):
-        self.async_on_remove(
-            self.hass.helpers.event.async_track_state_change(
-                self._source, self._state_changed
-            )
+        async_track_state_change_event(
+            self.hass, [self._source], self._state_changed
         )
 
-    async def _state_changed(self, entity_id, old_state, new_state):
+    async def _state_changed(self, event):
+        new_state = event.data.get("new_state")
+
+        if new_state is None:
+            return
+
         try:
             value = float(new_state.state)
-            self._buffer.append(value)
+        except ValueError:
+            return
 
-            if len(self._buffer) == WINDOW_SIZE:
-                freq, amp = dominant_frequency(
-                    list(self._buffer),
-                    SAMPLING_RATE
-                )
-                self._state = round(freq, 3)
-                self._amplitude = round(amp, 3)
-                self.async_write_ha_state()
+        self._buffer.append(value)
 
-        except Exception:
-            pass
+        if len(self._buffer) < self._buffer.maxlen:
+            return
+
+        try:
+            freq, amp = dominant_frequency(
+                list(self._buffer), self._sampling_rate
+            )
+
+            self._freqs, self._fft_vals = compute_fft(
+                list(self._buffer), self._sampling_rate
+            )
+
+            self._state = round(freq, 4)
+            self._amplitude = round(amp, 4)
+
+            self.async_write_ha_state()
+
+        except Exception as e:
+            _LOGGER.error(f"FFT calculation error: {e}")
 
     @property
     def state(self):
@@ -44,5 +58,6 @@ class FFTSensor(SensorEntity):
     def extra_state_attributes(self):
         return {
             "amplitude": self._amplitude,
-            "samples": len(self._buffer)
+            "samples": len(self._buffer),
+            "sampling_rate": self._sampling_rate
         }
